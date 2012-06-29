3cc5f23a87b9f93d75a72bc411d614e077505ab1
bogdan <bogdan@itechcraft.com>
2012-06-29 16:58:26 +0300

    Runner initialisation improved and moved to module

--- CmdrunModule.php
+++ CmdrunModule.php
@@ -2,19 +2,51 @@
 
 class CmdrunModule extends CWebModule
 {
-	public function init()
-	{
-		// this method is called when the module is being created
-		// you may place code here to customize the module or the application
+    /**
+     * @var CConsoleCommandRunner 
+     */
+    public $runner;
+    
+    /**
+     * @var array console application config
+     */
+    public $config;
 
+    public function init()
+	{
+        // This module is for debug only
+        if (!YII_DEBUG)
+            throw new CHttpException(403);
+        
 		// import the module-level models and components
 		$this->setImport(array(
 			'cmdrun.models.*',
 			'cmdrun.components.*',
 		));
+        
+        $this->config = include Yii::getPathOfAlias('application.config.console') . '.php';
+        
+        $this->_initRunner();
 	}
 
-	public function beforeControllerAction($controller, $action)
+    private function _initRunner()
+    {
+        $this->runner = new CConsoleCommandRunner();
+        
+        // Adding commandMap
+        if ( isset($this->config['commandMap']) )
+            $this->runner->commands = $this->config['commandMap'];
+        
+        // Adding commands path
+        if ( isset($this->config['commandPath']) )
+            $path = $this->config['commandPath'];
+        else
+            $path = 'protected/commands';
+        
+        $this->runner->addCommands( $path );
+    }
+
+    public function beforeControllerAction($controller, $action)
 	{
 		if(parent::beforeControllerAction($controller, $action))
 		{
--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -1,30 +1,12 @@
 <?php
 
+/**
+ * @property CmdrunModule $module 
+ * 
+ * @property-read CConsoleCommandRunner $runner
+ */
 class DefaultController extends Controller
 {
-    /**
-     * @var CConsoleCommandRunner 
-     */
-    protected $_runner;
-
-    protected $_alias = 'application.commands';
-
-    
-    public function init()
-    {
-        parent::init();
-        
-        $this->_runner = new CConsoleCommandRunner();
-    }
-
-    public function beforeAction($action)
-    {
-        // This controller is for debug only
-        if (!YII_DEBUG)
-            throw new CHttpException(403);
-        
-        return parent::beforeAction($action);
-    }
     
     public function actionIndex()
     {
@@ -47,7 +29,7 @@ class DefaultController extends Controller
             '<h2>Output</h2>' .
             '<pre>';
         
-        $this->_runner->run(array('', $name) + $attrs);
+        $this->runner->run(array('', $name) + $attrs);
         
         echo
             '</pre>';
@@ -56,12 +38,18 @@ class DefaultController extends Controller
     
     public function showCommands()
     {
-        $this->_runner->addCommands( Yii::getPathOfAlias($this->_alias) );
-        
         $links = array();
-        foreach ($this->_runner->commands as $name => $file) {
+        foreach ($this->runner->commands as $name => $file) {
             $links[] = CHtml::link($name, $this->createUrl('run', array('name' => $name)));
         }
         echo '<p>Commands: ' . implode(', ', $links) . '</p>';
+    }
+    
+    /**
+     * @return CConsoleCommandRunner 
+     */
+    public function getRunner()
+    {
+        return $this->module->runner;
     }
 }
